2bca1df438d7042003d813149fb9cbe3fae461f0
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-16 22:36:00 +0300

    Task 1_1

---
+++ script.js
@@ -0,0 +1,29 @@
+const Calculator = function (digit1, digit2) {
+    this.digit1 = digit1;
+    this.digit2 = digit2;
+    this.inWork = false;
+    this.operation = prompt('Введите математическое действие: +, -, /, *')
+    this.doCalculate = function(){
+        let result = 0;
+        switch (this.operation){
+            case '+':
+                result = this.digit1 + this.digit2;
+            break;
+            case '-':
+                result = this.digit1 - this.digit2;
+                break;
+            case '*':
+                result = this.digit1 * this.digit2;
+                break;
+            case '/':
+                result = this.digit1 / this.digit2;
+                break;
+            default:
+                return document.write('Вы ввели неправильное действие')
+        }
+        return document.write(result);
+    }
+}
+
+const Sum = new Calculator(8,2);
+Sum.doCalculate();
