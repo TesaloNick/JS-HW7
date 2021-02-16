e10d74e57217368cc382807ce1b5ab49389ffa05
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-16 22:41:10 +0300

    Task 1_2

--- script.js
+++ script.js
@@ -3,27 +3,39 @@ const Calculator = function (digit1, digit2) {
     this.digit2 = digit2;
     this.inWork = false;
     this.operation = prompt('Введите математическое действие: +, -, /, *')
+    this.start = function(){
+        this.inWork = true;
+    }
+    this.finish = function(){
+        this.inWork = false;
+    }
     this.doCalculate = function(){
         let result = 0;
-        switch (this.operation){
-            case '+':
-                result = this.digit1 + this.digit2;
-            break;
-            case '-':
-                result = this.digit1 - this.digit2;
+        if (this.inWork === false){
+            return document.write('Калькулятор выключен')
+        } else {
+            switch (this.operation){
+                case '+':
+                    result = this.digit1 + this.digit2;
                 break;
-            case '*':
-                result = this.digit1 * this.digit2;
-                break;
-            case '/':
-                result = this.digit1 / this.digit2;
-                break;
-            default:
-                return document.write('Вы ввели неправильное действие')
+                case '-':
+                    result = this.digit1 - this.digit2;
+                    break;
+                case '*':
+                    result = this.digit1 * this.digit2;
+                    break;
+                case '/':
+                    result = this.digit1 / this.digit2;
+                    break;
+                default:
+                    return document.write('Вы ввели неправильное действие')
+            }
+            return document.write(result);
         }
-        return document.write(result);
     }
 }
 
-const Sum = new Calculator(8,2);
-Sum.doCalculate();
+const sum = new Calculator(8,2);
+sum.start();
+sum.doCalculate();
+sum.Finish();
